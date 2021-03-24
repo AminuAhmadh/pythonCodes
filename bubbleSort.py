17ae8ad7c698d8a41b812dfe2ab33f6ec3471642
= Aminu Ahmadh <= ameenuhamad@gmail.com>
2021-03-24 11:36:54 +0100

    modified bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,16 +1,13 @@
 # bubble sort
 
+
 def bubble_sort(numList):
-    for i in range(len(numList) -1 ,0, -1):
-        for j in range(i):
+    iteration = len(numList) -1
+    for i in range(iteration):
+        for j in range(iteration):
             if numList[j] > numList[j+1]:
                 temp = numList[j]
                 numList[j] = numList[j+1] 
                 numList[j+1] = temp
-
     return numList
-
-
-
-print(bubble_sort([6,2,5,8,1,9]))
-
+print(bubble_sort([9,7,8,6,4,5,2,0,1,3,33]))
