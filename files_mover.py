3fe7c27fc27d3170f154055565844828b23ac2fa
= Aminu Ahmadh <= ameenuhamad@gmail.com>
2021-08-23 10:19:08 +0100

    added a file

--- files_mover.py
+++ files_mover.py
@@ -4,7 +4,7 @@ import os
 from zipfile import ZipFile
 
 
-# point to the Downloads Folder/path
+# point to the Downloads Folder
 downloads_path = os.path.expanduser('~\\Downloads')
 os.chdir(downloads_path)
 
@@ -17,23 +17,29 @@ music_dst = os.path.expanduser('~\\Music')
 
 
 for root, dirs, files in os.walk(downloads_path, topdown=False):
-    files = [f.lower() for f in files if not f[0] == '.']
     for file in files:
-        # if it's a video
-        if file.endswith(('mp4', 'avi', 'mov', 'wmv', 'flv', 'mkv')):
-            # move to desired destination
-            shutil.move(file, videos_dst)
-        # if it is a picture
-        elif file.endswith(('png', 'jpeg')):
-            # move it
-            shutil.move(file, pictures_dst)
-        # else if they're docs
-        elif file.endswith(('doc', 'docx', 'txt', 'ppt','pptx', 'epub', 'pdf', 'xls', 'xlsx')):
-            # move
-            shutil.move(file, docs_dst)
-        elif file.endswith(('mp3', 'wav')):
-            # move it
-            shutil.move(file, music_dst)
-        else:
-            pass
+        try:
+            # if it's a video
+            if file.lower().endswith(('mp4', 'avi', 'mov', 'wmv', 'flv', 'mkv')):
+                # move to desired destination
+                shutil.move(file, videos_dst)
+                print('moved a video')
+            # if it is a picture
+            elif file.lower().endswith(('png', 'jpeg')):
+                # move it
+                shutil.move(file, pictures_dst)
+                print('moved a picture')
+            # else if they're docs
+            elif file.lower().endswith(('doc', 'docx', 'txt', 'ppt','pptx', 'epub', 'pdf', 'xls', 'xlsx')):
+                # move
+                shutil.move(file, docs_dst)
+                print('moved a document')
+            elif file.lower().endswith(('mp3', 'wav')):
+                # move it
+                shutil.move(file, music_dst)
+                print('moved an audio')
+            else:
+                pass
+        except Exception as e:
+            print (e)
 print('Done Moving.')
