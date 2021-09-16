afb9a2ded2fd26df670b986f253c0da79a180c93
= Aminu Ahmadh <= ameenuhamad@gmail.com>
2021-09-16 18:26:26 +0100

    edit dice rolling

--- diceRollin.py
+++ diceRollin.py
@@ -2,55 +2,56 @@
 # By Aminu Ahmadh
 # GITHUB @AminuAhmadh
 
-
+# lmport some libraries
 import random, time, pyinputplus
-import pandas
+import seaborn as se
+
 
 print('YOU ARE PLAYING WITH A COMPUTER')
 print("THE FIRST TO REACH '50' WINS")
 print()
 
-win = pandas.Series ({'MY WINS': 0, 'COMPUTER WINS': 0 })
-while True:
-    myWin = 0
-    computerWin = 0
+# dict to store progress
+scores = dict({'Your Wins': 0, 'Computer Wins': 0})
 
-    question = pyinputplus.inputYesNo('wanna roll a dice? \n')
-    if question == 'yes':
-        for i in range(50):
-            print()
-            roll = pyinputplus.inputChoice(['roll', 'r'], prompt='Enter "ROLL" or "R" to roll: ').lower()
-            print()
-            if roll == 'roll' or 'r':
-                dice_roll = random.randint(1,6)
-                print(f'you rolled: {dice_roll} ')
-                myWin += dice_roll
-                print('my progress:',myWin)
+# call to roll
+def roll():
+    rolled = random.randint(1,6)
+    return rolled
+
+while True:
+    question = pyinputplus.inputYesNo('Are You Ready? \n')
+    if question.lower() == 'no':
+        break
+    else:
+        while True:
+            print("Press Any Key to Roll")
+            prompt = pyinputplus.inputStr()
+            if prompt:
+                my_roll = roll()
+                scores['Your Wins'] += my_roll
+                print("YOU ROLLED:", my_roll)
+                time.sleep(1)
+                print(scores)
+                time.sleep(1)
                 print()
-                time.sleep(0.5)
-                comp_dice = random.randint(1,6)
-                print(f'Computer rolled: {comp_dice} ')
-                computerWin += comp_dice
-                print(f'computer progress:',computerWin)
-                if myWin >= 50:
-                    print()
-                    print('You Won! Congratulations.')
-                    win['MY WINS'] += 1
+                if scores['Your Wins'] >= 50:
+                    print("Yay! You Won.")
                     break
-                elif computerWin >= 50:
-                    print()
-                    print('Computer Won!')
-                    win['COMPUTER WINS'] += 1
+                comp_roll = roll()
+                scores['Computer Wins'] += comp_roll
+                print("COMPUTER ROLLED:", comp_roll)
+                time.sleep(1)
+                print(scores)
+                time.sleep(1)
+                print()
+                if scores['Computer Wins'] >= 50:
+                    print("Nay! You Lost.")
                     break
                 continue
-
-    elif question == 'no':
-        exit
-    print()
-    print('My total wins:', str(win['MY WINS']))
-    print('Computer total wins:', str(win['COMPUTER WINS']))
-    roll_again = pyinputplus.inputYesNo(prompt='wanna roll again? ')
-    if roll_again =='y':
+    play_again = pyinputplus.inputYesNo('Play Again? \n')
+    if play_again.lower() == 'yes':
+        scores = dict({'Your Wins': 0, 'Computer Wins': 0})
         continue
     else:
         break
