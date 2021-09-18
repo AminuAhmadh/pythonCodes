301ba27f9acd118e82c209d970c017f4b4bad660
= Aminu Ahmadh <= ameenuhamad@gmail.com>
2021-09-18 11:31:53 +0100

    update linear search

--- linearSearch.py
+++ linearSearch.py
@@ -3,13 +3,12 @@
 
 
 def linearSearch(list, n): # list to search and number to find
-    index = 0
     for i in range(len(list)): # iterate through the list
         if list[i] == n: # if value of i equals n
-            index = i # assign index
+            index = list.index(list[i]) # assign index
             print('Found it at index', index+1)
             exit()
     print('Not Found') # not found
 
 
-linearSearch([1,2,5,], 2)
+linearSearch([1,2,5,8,6], 8)
