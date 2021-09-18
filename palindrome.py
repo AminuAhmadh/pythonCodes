3781a158a6a5caab5ad80744900ae8c0cb71ed2f
= Aminu Ahmadh <= ameenuhamad@gmail.com>
2021-09-18 11:17:32 +0100

    updated palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,19 @@
-words = input('Give me a list of five words separated by space: ').lower()
-count =0
+"""
+A word is a palindrome if it has thesame spelling when reversed. Like "MOM".
+"""
+
+
+words = input('Give me a list of words separated by space: ').lower()
+palindrome_count = 0
+not_palindrome = 0
 for word in words.split():
     if word[::-1] == word:
         print(word, 'is a palindrome')
-        count+=1
+        palindrome_count+=1
     else:
         print(word, 'is not a palindrome!')
-print(str(count), 'words are/is palindrome and the rest are not')
+        not_palindrome += 1
+print()
+print(str(palindrome_count), 'word(s) are palindrome.')
+print(str(not_palindrome), 'word(s) are not Palindrome.')
 
